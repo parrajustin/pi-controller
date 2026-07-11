ae6854147196376e0844860491c17228b35bf439
Justin Parra <1360989+parrajustin@users.noreply.github.com>
2026-07-11 16:58:17 -0700

    release: move kiosk and release

--- pkg/kiosk/release.sh
+++ pkg/kiosk/release.sh
@@ -23,18 +23,14 @@ NEW_VERSION=$((CURRENT_VERSION + 1))
 jq --arg v "$NEW_VERSION" '.version = ($v | tonumber)' "$VERSION_FILE" > version.tmp.json && mv version.tmp.json "$VERSION_FILE"
 
 TAGNAME="v${NEW_VERSION}"
-IMAGE_NAME="xerofuzzion/lounge_display_server"
+IMAGE_NAME="xerofuzzion/lounge_kiosks"
 
 echo "Building and pushing version ${TAGNAME}-x86_64 and latest-x86_64..."
 docker buildx build \
-  --build-context standard_ts_lib=../../../standard-ts-lib \
-  --build-context token_receiver=../token_receiver \
   --platform linux/amd64 -t "${IMAGE_NAME}:${TAGNAME}-x86_64" -t "${IMAGE_NAME}:latest-x86_64" --output type=image,push=true,compression=zstd,force-compression=true,compression-level=3 .
 
 echo "Building and pushing version ${TAGNAME}-aarch64 and latest-aarch64..."
 docker buildx build \
-  --build-context standard_ts_lib=../../../standard-ts-lib \
-  --build-context token_receiver=../token_receiver \
   --platform linux/arm64 -t "${IMAGE_NAME}:${TAGNAME}-aarch64" -t "${IMAGE_NAME}:latest-aarch64" --output type=image,push=true,compression=zstd,force-compression=true,compression-level=3 .
 
 echo "Successfully built and pushed multi-arch images for ${TAGNAME}"
